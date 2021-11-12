98a0e3794ba641e7f756b2dc15ba8511e59292b8
Prince-Thind <princethind799@gmail.com>
2021-11-12 15:59:48 +0530

    improve code logic

--- main.js
+++ main.js
@@ -1,70 +1,64 @@
 const keypad = document.querySelector("#keypad");
 const audioBar = document.querySelector("#audio");
 
-const letters = "a,s,d,f,g,h,j,k,l".split(",");
-const audioUrls = "boom,hihat,openhat,snare,tom,clap,kick,ride,tink"
-  .split(",")
-  .map((e) => "./assets/sounds/" + e + ".wav");
+const sounds = {};
 
-const audioDivs = [];
-const keyDivs = [];
-
-document.addEventListener("keydown", main);
-keypad.addEventListener("click", main);
+document.addEventListener("keydown", playPressedKey);
+keypad.addEventListener("click", playClicked);
 
 init();
 
 function init() {
+  const letters = "a,s,d,f,g,h,j,k,l".split(",");
+  const audioUrls = "boom,hihat,openhat,snare,tom,clap,kick,ride,tink"
+    .split(",")
+    .map((e) => "./assets/sounds/" + e + ".wav");
+
   for (let i = 0; i < letters.length; i++) {
-    generateDiv(keypad, letters[i]);
-    generateAudio(audioBar, letters[i], i);
+    const keyDiv = generateDiv(keypad, letters[i]);
+    const audioDiv = generateAudio(audioBar, audioUrls[i]);
+    addSound(letters[i], keyDiv, audioDiv);
+  }
+
+  function addSound(letter, div, audio) {
+    sounds[letter] = { div, audio };
   }
 
   function generateDiv(parent, letter) {
     const key = document.createElement("div");
     key.classList.toggle("key");
-    key.setAttribute("data-key", letter);
     key.innerText = letter;
-    // key.addEventListener('click', main);
-    keyDivs.push(key);
     parent.appendChild(key);
+    return key;
   }
-  function generateAudio(parent, letter, index) {
-    const file = document.createElement("audio");
-    file.setAttribute("data-key", letter);
-    file.setAttribute("src", audioUrls[index]);
-    audioDivs.push(file);
-    parent.appendChild(file);
+  function generateAudio(parent, src) {
+    const audioElement = document.createElement("audio");
+    audioElement.setAttribute("src", src);
+    parent.appendChild(audioElement);
+    return audioElement;
   }
 }
 
-function main(e) {
-  let keypressed = null;
-  let keyDiv = null;
-
-  // console.log(e.target.textContent + "|||" + e.target.innerText);
-
-  if (letters.includes(e.target.textContent)) {
-    keyDiv = e.target;
-    keypressed = e.target.textContent;
-  } else {
-    keypressed = String.fromCharCode(e.keyCode).toLowerCase();
-    keyDiv = keyDivs.find((div) => {
-      return div.getAttribute("data-key") == keypressed;
-    });
-  }
-  const file = audioDivs.find((div) => {
-    return div.getAttribute("data-key") == keypressed;
-  });
-
-  if (!file) return;
-
-  playTone(file, keyDiv);
+function playClicked(e) {
+  const element = e.target;
+  const pressed = element.textContent;
+  const audioElement = sounds[pressed]?.audio;
+  const keyElement = sounds[pressed]?.div;
+  playTone(audioElement, keyElement);
 }
 
-function playTone(file, key) {
-  file.currentTime = 0;
-  file.play();
+function playPressedKey(e) {
+  const element = e.target;
+  const pressed = String.fromCharCode(e.keyCode).toLowerCase();
+  const audioElement = sounds[pressed]?.audio;
+  const keyElement = sounds[pressed]?.div;
+  playTone(audioElement, keyElement);
+}
+
+function playTone(audioElement, key) {
+  if (!audioElement) return;
+  audioElement.currentTime = 0;
+  audioElement.play();
   key.classList.add("play");
   setTimeout(() => {
     key.classList.remove("play");
